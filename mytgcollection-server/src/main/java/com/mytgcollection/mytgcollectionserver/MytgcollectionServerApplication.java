2ba083d4a138e162ddfd704b61dc107c44db4fde
Andrei Appelt <andrei.appelt@gmail.com>
2020-08-12 23:40:04 -0300

    monorepo first commit

---
+++ mytgcollection-server/src/main/java/com/mytgcollection/mytgcollectionserver/MytgcollectionServerApplication.java
@@ -0,0 +1,13 @@
+package com.mytgcollection.mytgcollectionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytgcollectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytgcollectionServerApplication.class, args);
+	}
+
+}
---
+++ mytgcollection-server/src/test/java/com/mytgcollection/mytgcollectionserver/MytgcollectionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytgcollection.mytgcollectionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytgcollectionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
